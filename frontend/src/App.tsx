6b2e5de09bed14f0192953fb79ca558771f555d3
Matheus <matheusaraujo.dev@gmail.com>
2023-06-21 16:31:40 -0300

    Feat: fetch de dados sem React Query

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
+// uso da fake API https://reqres.in/
+
+export interface IData {
+  id: number
+  email: string
+  first_name: string
+  last_name: string
+  avatar: string
+}
+
+const getUser = async (id: number) => {
+  const request = await fetch(`https://reqres.in/api/users/${id}?delay=1`)
+  const response = await request.json()
+
+  if (!request.ok) {
+    throw new Error(response.error)
+  }
+
+  return response.data as IData
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentUserId, setCurrentUserId] = useState(1)
+  const [user, setUser] = useState<IData>()
+  const [loading, setLoading] = useState(false)
+  const [isError, setIsError] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+    getUser(currentUserId)
+      .then((res) => {
+        setUser(res)
+        setLoading(false)
+      })
+      .catch(() => {
+        setIsError(true)
+        setLoading(false)
+      })
+  }, [currentUserId])
+
+  if (isError) {
+    return (
+      <section>
+        <p>Something went wrong 😕</p>
+      </section>
+    )
+  }
+
+  if (!user || loading) {
+    return (
+      <section>
+        <p>Loading ...</p>
+      </section>
+    )
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <section>
+      <img src={user.avatar} alt="Profile Picture" />
+      <p>
+        {user.first_name}, {user.last_name}
       </p>
-    </>
+      <p>{user.email}</p>
+
+      <div>
+        <button onClick={() => setCurrentUserId((prev) => prev - 1)}>
+          Prev
+        </button>
+        <button onClick={() => setCurrentUserId((prev) => prev + 1)}>
+          Next
+        </button>
+      </div>
+    </section>
   )
 }
 
